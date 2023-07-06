2cddb64670f07a3c543f4e741c94e0c03c6167f5
David Forrester <david.o.forrester@gmail.com>
2023-07-06 20:34:23 +1000

    progress on the buildTree function

---
+++ bst.js
@@ -0,0 +1,51 @@
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  buildTree(arr, start, end) {
+    if (start > end) {
+      return null;
+    }
+    let mid = parseInt((start + end) / 2);
+
+    const node = new Node(arr[mid]);
+
+    node.left = this.buildTree(arr, start, mid - 1);
+    node.right = this.buildTree(arr, mid + 1, end);
+
+    return node;
+  }
+}
+
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new Tree();
+let root =tree.buildTree(arr, 0, arr.length - 1);
+
+prettyPrint(root)
+
+
+
+
