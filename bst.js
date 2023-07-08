652218b8430439bf2c8e49317ceffba3ef4b93dd
David Forrester <david.o.forrester@gmail.com>
2023-07-08 16:36:59 +1000

    working tree builder function

--- bst.js
+++ bst.js
@@ -5,7 +5,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
@@ -40,12 +40,9 @@ class Tree {
 }
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+//arr = [1, 7, 4, 23];
 
 const tree = new Tree();
-let root =tree.buildTree(arr, 0, arr.length - 1);
+let builtTree = tree.buildTree(arr, 0, arr.length - 1);
 
-prettyPrint(root)
-
-
-
-
+prettyPrint(builtTree);
