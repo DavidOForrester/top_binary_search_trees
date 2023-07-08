e2ebc3de71ff9fb4719cf213b16dac5dbaca857e
David Forrester <david.o.forrester@gmail.com>
2023-07-08 17:35:31 +1000

    balanced the tree

--- bst.js
+++ bst.js
@@ -11,6 +11,20 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
+function sortArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -24,25 +38,36 @@ class Tree {
     this.root = null;
   }
 
-  buildTree(arr, start, end) {
-    if (start > end) {
-      return null;
+  buildTree(arr) {
+    function build(arr, start, end) {
+      if (start > end) {
+        return null;
+      }
+      let mid = parseInt((start + end) / 2);
+
+      const node = new Node(arr[mid]);
+
+      node.left = build(arr, start, mid - 1);
+      node.right = build(arr, mid + 1, end);
+
+      return node;
     }
-    let mid = parseInt((start + end) / 2);
 
-    const node = new Node(arr[mid]);
+    const set = new Set(arr);
+    const uniqueArray = Array.from(set);
+    const sortedArray = sortArray(uniqueArray)
 
-    node.left = this.buildTree(arr, start, mid - 1);
-    node.right = this.buildTree(arr, mid + 1, end);
-
-    return node;
+    return build(sortedArray, 0, sortedArray.length - 1);
   }
+
+  insert(value) {}
+
+  delete(value) {}
 }
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-//arr = [1, 7, 4, 23];
 
 const tree = new Tree();
-let builtTree = tree.buildTree(arr, 0, arr.length - 1);
+let builtTree = tree.buildTree(arr);
 
 prettyPrint(builtTree);
